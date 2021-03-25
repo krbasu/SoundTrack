279c9ccb4356de9714342186c3f0b6119c189ff2
krbasu <64273116+krbasu@users.noreply.github.com>
2021-03-26 03:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.addEventListener("keydown", (e) => {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const box = document.querySelector(`.box[data-key="${e.keyCode}"]`);
+	audio.currentTime = 0.02;
 	audio.play();
 	box.classList.add("box-shadow");
 });
